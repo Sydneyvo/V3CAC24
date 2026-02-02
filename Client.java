d17d79b3c2fa193de7e6ad0decfbed6ca295f4ff
Sydneyvo <102885971+Sydneyvo@users.noreply.github.com>
2026-02-02 10:52:16 -0800

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,35 +1,36 @@
-import java.awt.*;
 import java.util.*;
 
 public class Client {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.println("Welcome to the CSE 123 Mondrian Art Generator!");
+        AbstractStrategyGame game = /* TODO - construct your game OR test out TicTacToe */;
 
-        int choice = 0;
-        while (choice != 1 && choice != 2) {
-            System.out.print("Enter 1 for a basic Mondrian or 2 for a complex Mondrian: ");
-            choice = console.nextInt();
+        System.out.println(game.instructions());
+        System.out.println();
+
+        while (!game.isGameOver()) {
+            System.out.println(game);
+            System.out.printf("Player %d's turn.\n", game.getNextPlayer());
+            try {
+                game.makeMove(game.getMove(console));
+            } catch (IllegalArgumentException ex) {
+                System.out.println("**Illegal move: " + ex.getMessage());
+            }
+            /**
+             * Note - the above structure is a try/catch, which is something
+             * we've included to help deal with the IllegalArgumentExceptions
+             * in your abstract strategy game!
+             * We want to remind you that try/catch is a forbidden feature in 123,
+             * so you SHOULD NOT INCLUDE IT in any code you submit (other than this file)!
+             * Please see the Code Quality Guide for more info on this.
+             */
         }
-        System.out.print("Enter image width (>= 300px): ");
-        int width = console.nextInt();
-        System.out.print("Enter image height (>= 300px): ");
-        int height = console.nextInt();
-
-        Mondrian mond = new Mondrian();
-        Picture pic = new Picture(width, height);
-        Color[][] pixels = pic.getPixels();
-
-        if (choice == 1) {
-            mond.paintBasicMondrian(pixels);
-        } else {    // choice == 2
-            mond.paintComplexMondrian(pixels);
+        System.out.println(game);
+        int winner = game.getWinner();
+        if (winner > 0) {
+            System.out.printf("Player %d wins!\n", winner);
+        } else {
+            System.out.println("It's a tie!");
         }
-        
-        pic.setPixels(pixels);
-        pic.save(choice == 1 ? "basic.png" : "extension.png");
-        pic.show();
-        System.out.println("Enjoy your artwork!");
     }
 }
-
